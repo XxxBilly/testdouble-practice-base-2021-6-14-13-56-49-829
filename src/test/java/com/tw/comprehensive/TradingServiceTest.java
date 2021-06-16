99bc4d6f8e6035c0da32f87935a303097abcdfb7
XxxBilly <1363543881@qq.com>
2021-06-16 19:23:10 +0800

    complete task one

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -1,7 +1,29 @@
 package com.tw.comprehensive;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
 
 class TradingServiceTest {
+
+  @Test
+  void should_call_logNewTrade_when_execute_createTrade() {
+    // mock
+    TradeRepository tradeRepository = spy(TradeRepository.class);
+    when(tradeRepository.createTrade(any())).thenReturn(null);
+    AuditService auditService = mock(AuditService.class);
+
+    // when
+    TradingService tradingService = new TradingService(tradeRepository, auditService);
+    tradingService.createTrade(null);
+
+    // then
+    verify(auditService, times(1)).logNewTrade(any());
+  }
 
 }
